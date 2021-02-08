6381a7f818544ec287791aea8280d4b16278cf87
Yaser.AndOr <yaser1222@hotmail.com>
2021-02-08 08:10:06 -0600

    Guia4

---
+++ Guía4Ejercicio1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Tot = 0, Resp = 0, Resul = 0;
+            Console.WriteLine("Ingrese la cantidad de preguntas totales:");
+            Tot = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de preguntas respondidas:");
+            Resp = Double.Parse(Console.ReadLine());
+            Resul = (Resp * 100) / Tot;
+            if (Tot >= 90)
+            {
+                Console.WriteLine("El empleado se encuentra en el nivel máximo");
+            }
+            else if (Tot >= 75 && Tot < 90)
+            {
+                Console.WriteLine("El empleado se encuentra en el nivel medio");
+            }
+            else if (Tot >= 50 && Tot <75)
+            {
+                Console.WriteLine("El empleado se encuentra en el nivel regular");
+            }
+            else if (Tot < 50)
+            {
+                Console.WriteLine("El empleado se encuentra fuera de nivel");
+            }
+            Console.ReadKey();
+        }
+    }
+}
